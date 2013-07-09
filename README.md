299dc4aa907e795af2db5cbaf23ccdd1ee10b53c
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-07-09 01:51:26 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#CharLimit for [Brackets.io](https://github.com/adobe/brackets)
+
+Draws a gray line at 80 characters. May not be visible in some themes.
---
+++ main.js
@@ -0,0 +1,53 @@
+/*jslint indent: 2,maxerr: 50 */
+/*global define, brackets, AppInit */
+
+define(function (require, exports, module) {
+  "use strict";
+
+  var DocumentManager = brackets.getModule("document/DocumentManager"),
+      EditorManager   = brackets.getModule("editor/EditorManager");
+
+  function charWidth() {
+    var o = $('<div>a</div>')
+      .css({'position': 'absolute',
+            'float': 'left',
+            'white-space': 'nowrap',
+            'visibility': 'hidden'})
+      .appendTo($('body'));
+    var w = o.width();
+    o.remove();
+    return w;
+  }
+  
+  function getGutterWidth() {
+    var result = 15;
+    var max = 0;
+    $('.CodeMirror-gutters').each(function() {
+      if ($(this).width() > max) {
+        max = $(this).width();
+      }
+    });
+    result += max;
+    return result;
+  }
+
+  function drawCharLimit() {
+    $('#charlimit').remove();
+    var cols = 80;
+    cols--;
+    var width = cols*charWidth() + getGutterWidth();
+    var editorHolder = $('#editor-holder');
+    editorHolder.append('<div id="charlimit" class="charlimit"></div>');
+    var charlimit = $('#charlimit');
+    charlimit.css('position', 'fixed');
+    charlimit.css('top', '0');
+    charlimit.css('height', '100%');
+    charlimit.css('width', width + 'px');
+    charlimit.css('border-right', 'thin solid #bbb');
+    charlimit.css('pointer-events', 'none');
+  }
+
+  $(DocumentManager).on('currentDocumentChange', drawCharLimit);
+});
+
+///////////////////////////////////////////////////////////////////////////////
