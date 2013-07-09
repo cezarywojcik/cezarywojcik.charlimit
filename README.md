23def1d5fb08073c510a507eb4feca23f48ca22b
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-07-09 02:55:24 -0700

    added settings screen

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 #CharLimit for [Brackets.io](https://github.com/adobe/brackets)
 
-Draws a gray line at 80 characters. May not be visible in some themes.
 
-Not yet implemented: ability to adjust the line to a different value than 80 characters.
 
 ## Known Issues
 
-If [Interactive Linter](https://github.com/MiguelCastillo/Brackets-Interactive-Linter) is also installed, on the first load, the line is too far to the left because the interactive linter gutter hasn't loaded.
+If [Interactive Linter](https://github.com/MiguelCastillo/Brackets-Interactive-Linter) is also installed, on the first load, the line can be too far to the left if there are any lint messages because the interactive linter gutter hasn't loaded.
--- main.js
+++ main.js
@@ -1,10 +1,50 @@
 /*jslint indent: 2,maxerr: 50 */
-/*global define, brackets */
+/*global define, brackets, Mustache, localStorage */
 
 define(function (require, exports, module) {
   "use strict";
 
-  var DocumentManager = brackets.getModule("document/DocumentManager");
+  var  CommandManager  = brackets.getModule("command/CommandManager"),
+       Menus           = brackets.getModule("command/Menus"),
+       DocumentManager = brackets.getModule("document/DocumentManager"),
+       Strings         = brackets.getModule("strings"),
+       Dialogs         = brackets.getModule("widgets/Dialogs"),
+       PrefsTemplate   = require("text!preferences.html");
+
+  var preferenceKeyId = 'charlimit.preferences';
+
+  var defaultPreferences = {
+    charLimitCols: 80,
+    charLimitColor: '#bbbbbb',
+    charLimitEnabled: true
+  };
+
+  function getStorage() {
+    var userData = localStorage.getItem(preferenceKeyId);
+    return userData ? JSON.parse(userData) : $.extend({}, defaultPreferences);
+  }
+
+  function getPreference(name) {
+    return getStorage()[name];
+  }
+
+  function saveStorage(storage) {
+    localStorage.setItem(preferenceKeyId, JSON.stringify(storage));
+  }
+
+  function setPreference(name, value) {
+    var storage = getStorage();
+    if (typeof name == 'object') {
+      for (var p in name) {
+        if (name.hasOwnProperty(p)) {
+          storage[p] = name[p];
+        } else {
+          storage[name] = value;
+        }
+      }
+      saveStorage(storage);
+    }
+  }
 
   function charWidth() {
     var o = $('<div>a</div>')
@@ -32,19 +72,56 @@ define(function (require, exports, module) {
 
   function drawCharLimit() {
     $('#charlimit').remove();
-    var cols = 80;
-    cols--;
-    var width = cols*charWidth() + getGutterWidth();
-    var editorHolder = $('#editor-holder');
-    editorHolder.append('<div id="charlimit" class="charlimit"></div>');
-    var charlimit = $('#charlimit');
-    charlimit.css('position', 'fixed');
-    charlimit.css('top', '0');
-    charlimit.css('height', '100%');
-    charlimit.css('width', width + 'px');
-    charlimit.css('border-right', 'thin solid #bbb');
-    charlimit.css('pointer-events', 'none');
+    if (getPreference('charLimitEnabled')) {
+      var cols = getPreference('charLimitCols');
+      cols--;
+      var width = cols*charWidth() + getGutterWidth();
+      var editorHolder = $('#editor-holder');
+      editorHolder.append('<div id="charlimit" class="charlimit"></div>');
+      var charlimit = $('#charlimit');
+      charlimit.css('position', 'fixed');
+      charlimit.css('top', '0');
+      charlimit.css('height', '100%');
+      charlimit.css('width', width + 'px');
+      var color = getPreference('charLimitColor');
+      charlimit.css('border-right', 'thin solid ' + color);
+      charlimit.css('pointer-events', 'none');
+    }
+  }
+
+  function showCharLimitPreferences() {
+    var promise = Dialogs
+      .showModalDialogUsingTemplate(Mustache.render(PrefsTemplate, Strings))
+      .done(function (id) {
+        if (id === Dialogs.DIALOG_BTN_OK) {
+          setPreference({
+            charLimitCols: colsInput.val(),
+            charLimitColor: colorInput.val(),
+            charLimitEnabled: enabledInput[0].checked
+          });
+          drawCharLimit();
+        }
+      });
+
+    var dialog = $("#charlimit-settings");
+    var colsInput = dialog.find('#charlimit-cols');
+    var colorInput = dialog.find('#charlimit-color');
+    var enabledInput = dialog.find('#charlimit-enabled');
+
+    colsInput.val(getPreference('charLimitCols'));
+    colorInput.val(getPreference('charLimitColor'));
+    enabledInput[0].checked = !!getPreference('charLimitEnabled');
+
+    return promise;
   }
 
   $(DocumentManager).on('currentDocumentChange', drawCharLimit);
+
+  var CHARLIMIT_ID = "cezarywojcik.charlimitpref";
+  CommandManager.register("CharLimit Preferences", CHARLIMIT_ID,
+    showCharLimitPreferences);
+
+  var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+  menu.addMenuDivider();
+  menu.addMenuItem(CHARLIMIT_ID);
 });
